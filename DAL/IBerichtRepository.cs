4a5cfea176c902c30e408e1d02115c5d1a16a981
Brent <Brent@BRENT>
2018-03-18 23:51:16 +0100

    origin

---
+++ DAL/IBerichtRepository.cs
@@ -0,0 +1,55 @@
+﻿using BL.Domain;
+using BL.Domain.BerichtKlassen;
+using BL.Domain.ItemKlassen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   public interface IBerichtRepository
+   {
+      Bericht CreateBericht(Bericht bericht);
+      IEnumerable<Bericht> ReadBerichten();
+      Bericht ReadBericht(string berichtID);
+      void UpdateBericht(Bericht bericht);
+      void DeleteBericht(string berichtID);
+
+      IEnumerable<Woord> ReadWoordenVanBericht(string berichtID);
+      IEnumerable<Url> ReadUrlsVanBericht(string berichtID);
+      IEnumerable<Mention> ReadMentionsVanBericht(string berichtID);
+      IEnumerable<Hashtag> ReadHashtagsVanBericht(string berichtID);
+
+      Woord CreateBerichtWoord(string berichtID, Woord woord);
+      Url CreateBerichtUrl(string berichtID, Url url);
+      Mention CreateBerichtMention(string berichtID, Mention mention);
+      Hashtag CreateBerichtHashtag(string berichtID, Hashtag hashtag);
+
+      Persoon CreatePersoon(Persoon persoon);
+      Persoon ReadPersoon(string naam);
+      IEnumerable<Persoon> ReadPersonen();
+      void DeletePersoon(string persoon);
+
+      Woord CreateWoord(Woord woord);
+      IEnumerable<Woord> ReadWoorden();
+      Woord ReadWoord(string woord);
+      void DeleteWoord(string woord);
+
+      Url CreateUrl(Url url);
+      IEnumerable<Url> ReadUrls();
+      Url ReadUrl(string url);
+      void DeleteUrl(string url);
+
+      Mention CreateMention(Mention mention);
+      IEnumerable<Mention> ReadMentions();
+      Mention ReadMention(string mention);
+      void DeleteMention(string mention);
+
+      Hashtag CreateHashtag(Hashtag hashtag);
+      IEnumerable<Hashtag> ReadHashtags();
+      Hashtag ReadHashtag(string hashtag);
+      void DeleteHashtag(string hashtag);
+   }
+}
---
+++ Domain/ItemKlassen/Persoon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Domain.ItemKlassen
+{
+   public class Persoon : Item
+   {
+      public Persoon() { }
+
+      public Persoon(string Naam)
+      {
+         this.Naam = Naam;
+      }
+
+      [Key]
+      public string Naam { get; set; }
+      
+      public ICollection<Bericht> Berichten { get; set; }
+
+      public override string ToString()
+      {
+         return Naam;
+      }
+   }
+}
