f463954cfd7365611bf1dbbb192ca88b19c4c7c8
Hofmans Jonas <jonas.hofmans@student.kdg.be>
2018-04-20 11:38:10 +0200

    layout_inloggen

--- MVCIntegratie/Controllers/HomeController.cs
+++ MVCIntegratie/Controllers/HomeController.cs
@@ -9,9 +9,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
+
 namespace MVCIntegratie.Controllers
 {
-   public partial class HomeController : Controller
+    [RequireHttps]
+    public partial class HomeController : Controller
    {
       private IBerichtManager berichtMng = new BerichtManager();
       private IAlertManager alertMng = new AlertManager();
--- MVCIntegratie/Models/AccountViewModels.cs
+++ MVCIntegratie/Models/AccountViewModels.cs
@@ -48,12 +48,10 @@ namespace MVCIntegratie.Models
 
   public class LoginViewModel
   {
-    [Required]
     [Display(Name = "Email")]
     [EmailAddress]
     public string Email { get; set; }
 
-    [Required]
     [DataType(DataType.Password)]
     [Display(Name = "Password")]
     public string Password { get; set; }
