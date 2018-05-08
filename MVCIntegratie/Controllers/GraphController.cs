8b5a3cbed3059539c62a17342f16ff186357d6ff
Brent <Brent@BRENT>
2018-05-08 12:50:51 +0200

    blabla

--- MVCIntegratie/Controllers/GraphController.cs
+++ MVCIntegratie/Controllers/GraphController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MVCIntegratie.Controllers
 {
@@ -20,6 +21,8 @@ namespace MVCIntegratie.Controllers
       // GET: Graph
       public virtual ActionResult Index()
       {
+         var gebruiker = Membership.GetUser();
+
          List<Persoon> personen = berichtMng.GetPersonen().ToList();
          personen.Sort((p1, p2) => p1.Naam.CompareTo(p2.Naam));
          
