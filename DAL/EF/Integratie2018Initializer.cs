e38475be73b88e63652a57440e5367130a083267
Brent <Brent@BRENT>
2018-05-10 10:37:38 +0200

    sync weggedaan

--- DAL/EF/Integratie2018Initializer.cs
+++ DAL/EF/Integratie2018Initializer.cs
@@ -27,15 +27,6 @@ namespace DAL
 
          AddFaq(context);
 
-
-         /*var sync = new Synchronize()
-         {
-            ID = 0,
-            Latest = new DateTime(2018, 1, 1),
-            Context = context
-         };
-         sync.Start();*/
-
          context.SaveChanges();
       }
 
