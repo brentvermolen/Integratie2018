9fe07beaaa6cf6f50b8263d9b64db9ebeef91ba8
Brent <Brent@BRENT>
2018-05-11 11:52:33 +0200

    admin paginas

--- MVCIntegratie/Controllers/ConfigController.cs
+++ MVCIntegratie/Controllers/ConfigController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BL;
+using BL.Domain;
+using MVCIntegratie.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,24 +9,32 @@ using System.Web.Mvc;
 
 namespace MVCIntegratie.Controllers
 {
-    public partial class ConfigController : Controller
-    {
-        // GET: Config
-        public virtual ActionResult AdminConfig()
-        {
-            return View();
-        }
-        public virtual ActionResult Gebruikers()
-        {
-            return View();
-        }
-        public virtual ActionResult Deelplatform()
-        {
-            return View();
-        }
-        public virtual ActionResult Admin()
-        {
-            return View();
-        }
-    }
+   public partial class ConfigController : Controller
+   {
+      private FYIManager FyiMng = new FYIManager();
+      private IGebruikerManager GebruikerMng = new GebruikerManager();
+
+      // GET: Config
+      public virtual ActionResult AdminConfig()
+      {
+         return View();
+      }
+      public virtual ActionResult Gebruikers()
+      {
+         return View();
+      }
+      public virtual ActionResult Deelplatform()
+      {
+         return View();
+      }
+      public virtual ActionResult Admin()
+      {
+         AdminModel model = new AdminModel()
+         {
+            FAQ = FyiMng.GetFAQs().OrderByDescending(f => f.GesteldOp).ToList(),
+            Gebruikers = GebruikerMng.GetGebruikers().OrderBy(g => g.Naam).ToList()
+         };
+         return View(model);
+      }
+   }
 }
--- MVCIntegratie/Scripts/GebruikerSelect.js
+++ MVCIntegratie/Scripts/GebruikerSelect.js
@@ -1,12 +1,1 @@
-﻿$(function () {
-    console.log('ready');
-
-    $('.list-group li').click(function (e) {
-        e.preventDefault()
-
-        $that = $(this);
-
-        $that.parent().find('li').removeClass('active');
-        $that.addClass('active');
-    });
-})
+﻿
