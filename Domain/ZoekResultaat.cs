5e245555b7d0b0190c8dd0e39f607bd0b3d007b8
Evelien <evelien.hullebroeck@student.kdg.be>
2018-05-22 23:16:12 +0200

    search update

---
+++ Domain/ZoekResultaat.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.Domain;
+using BL.Domain.BerichtKlassen;
+
+using System.Web;
+
+namespace BL.Domain
+{
+    
+    public class ZoekResultaat
+    {
+        public ZoekResultaat()
+        {
+            Woorden = new List<Woord>();
+            Urls = new List<Url>();
+            Mentions = new List<Mention>();
+            Hashtags = new List<Hashtag>();
+            Personen = new List<Persoon>();
+            Grafieken = new List<Grafiek>();
+
+        }
+
+
+        public List<Woord> Woorden { get; set; }
+        public List<Hashtag> Hashtags { get; set; }
+        public List<Mention> Mentions { get; set; }
+        public List<Url> Urls { get; set; }
+        public List<Grafiek> Grafieken { get; set; }
+        public List<Persoon> Personen { get; set; }
+    }
+
+    public enum ZoekFilter
+    {
+        Politieker,
+        Trefwoord,
+        Mention,
+        Tag,
+        Link,
+        Grafiek
+
+    }
+
+    public enum ZoekSorteren
+    {
+        AantalBerichten,
+        Naam
+    }
+}
--- MVCIntegratie/Controllers/SearchController.cs
+++ MVCIntegratie/Controllers/SearchController.cs
@@ -1,107 +1,286 @@
-﻿using BL;
-using BL.Domain;
-using BL.Domain.BerichtKlassen;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
+using BL;
+using BL.Domain;
+using BL.Domain.BerichtKlassen;
+using MVCIntegratie.Models;
+using Microsoft.AspNet.Identity;
+
 
 
 namespace MVCIntegratie.Controllers
 {
-   public partial class SearchController : Controller
-  {
-
-    private BerichtManager berichtMng = new BerichtManager();
-    private AlertManager alertMng = new AlertManager();
-    private GebruikerManager gebruikerMng = new GebruikerManager();
-
-
-
-    // GET: Search
-    public virtual ActionResult Index(string search)
+    public partial class SearchController : Controller
     {
-      List<Persoon> personen = berichtMng.GetPersonen().Where(p => p.Naam.ToLower().Contains(search.ToLower()))
-          .ToList();
 
-
-      string[] splitSearch = search.Split(' ');
-      List<Woord> woorden = new List<Woord>();
-      List<Hashtag> hashtags = new List<Hashtag>();
-      List<Mention> mentions = new List<Mention>();
-      List<Url> urls = new List<Url>();
-      // List<Thema> themas = new List<Thema>();
-
-      List<Bericht> berichten = new List<Bericht>();
-      Bericht zoekresultaat = new Bericht();
-      foreach (string wrd in splitSearch)
-      {
-        woorden.AddRange(berichtMng.GetWoorden().Where(w => w.Tekst.ToLower().Contains(wrd.ToLower()))
-            .ToList());
-        hashtags.AddRange(berichtMng.GetHashtags().Where(h => h.Tekst.ToLower().Contains(wrd.ToLower()))
-            .ToList());
-        mentions.AddRange(berichtMng.GetMentions().Where(m => m.Tekst.ToLower().Contains(wrd.ToLower()))
-            .ToList());
-        urls.AddRange(berichtMng.GetUrls().Where(u => u.Tekst.ToLower().Contains(wrd.ToLower())).ToList());
-      }
-
-      //personen.Sort();
-      zoekresultaat.Woorden = woorden;
-      zoekresultaat.Hashtags = hashtags;
-      zoekresultaat.Mentions = mentions;
-      zoekresultaat.Personen = personen;
-      zoekresultaat.Urls = urls;
-      //Sortering testSortering = new Sortering();
+        private BerichtManager berichtMng = new BerichtManager();
+        private AlertManager alertMng = new AlertManager();
+        private GebruikerManager gebruikerMng = new GebruikerManager();
+        private GrafiekenManager grafiekMng = new GrafiekenManager();
 
 
 
-      woorden.ToString();
-      return View(zoekresultaat);
+        // GET: Search
+        public virtual ActionResult Index(FormCollection formCollection)
+        {
+            formCollection.AllKeys.ToString();
+            string search = formCollection["search"];
+            string filter = formCollection["Filter"];
+            string sorter = formCollection["Sorteren"];
+            string startDate = formCollection["startDate"];
+            string endDate = formCollection["endDate"];
+
+            List<Bericht> berichten = new List<Bericht>();
+            ZoekResultaat zoekResultaat = new ZoekResultaat();
+            if (startDate.IsEmpty() || startDate == null)
+            {
+                startDate = "2017-01-01";
+            }
+            if (endDate.IsEmpty() || endDate == null)
+            {
+                endDate = DateTime.Now.ToShortDateString();
+            }
+
+
+
+
+
+            switch (filter)
+            {
+                case "Politieker":
+                    {
+
+                        zoekResultaat.Personen = zoekPersonen(search, startDate, endDate);
+
+
+                    }
+                    break;
+                case "Tag":
+                    {
+
+                        zoekResultaat.Hashtags = zoekHashtags(search, startDate, endDate);
+
+                    }
+                    break;
+                case "Trefwoord":
+                    {
+                        zoekResultaat.Woorden = zoekWoorden(search, startDate, endDate);
+
+
+                    }
+                    break;
+                case "Mention":
+                    {
+
+                        zoekResultaat.Mentions = zoekMentions(search, startDate, endDate);
+
+
+
+                    };
+                    break;
+                case "Link":
+                    {
+
+                        zoekResultaat.Urls = zoekUrls(search, startDate, endDate);
+
+
+                    };
+                    break;
+                case "Grafiek":
+                    {
+
+                        zoekResultaat.Grafieken = zoekGrafieken(search, startDate, endDate);
+
+                    };
+                    break;
+                default:
+                    {
+                        zoekResultaat.Woorden = zoekWoorden(search, startDate, endDate);
+                        zoekResultaat.Hashtags = zoekHashtags(search, startDate, endDate);
+                        zoekResultaat.Mentions = zoekMentions(search, startDate, endDate);
+                        zoekResultaat.Personen = zoekPersonen(search, startDate, endDate);
+                        zoekResultaat.Urls = zoekUrls(search, startDate, endDate);
+                        zoekResultaat.Grafieken = zoekGrafieken(search, startDate, endDate);
+                    }
+                    break;
+            }
+
+            switch (sorter)
+            {
+                case "AantalBerichten":
+                    {
+                        zoekResultaat.Woorden.Sort();
+                        zoekResultaat.Hashtags.Sort();
+                        zoekResultaat.Mentions.Sort();
+                        zoekResultaat.Personen.Sort((p1, p2) => p1.Berichten.Count().CompareTo(p2.Berichten.Count()));
+                        zoekResultaat.Urls.Sort();
+                    }; break;
+                case "Naam":
+                    {
+                        zoekResultaat.Woorden.Sort((w1, w2) => w1.Tekst.CompareTo(w2.Tekst));
+                        zoekResultaat.Hashtags.Sort((h1, h2) => h1.Tekst.CompareTo(h2.Tekst));
+                        zoekResultaat.Mentions.Sort((m1, m2) => m1.Tekst.CompareTo(m2.Tekst));
+                        zoekResultaat.Urls.Sort((u1, u2) => u1.Tekst.CompareTo(u2.Tekst));
+                        zoekResultaat.Personen.Sort((p1, p2) => p1.Naam.CompareTo(p2.Naam));
+
+                    };
+                    break;
+
+                default:
+                    {
+                        zoekResultaat.Woorden.Sort();
+                        zoekResultaat.Hashtags.Sort();
+                        zoekResultaat.Mentions.Sort();
+                        zoekResultaat.Personen.Sort((p1, p2) => p1.Berichten.Count().CompareTo(p2.Berichten.Count()));
+                        zoekResultaat.Urls.Sort();
+                    };
+                    break;
+            }
+
+            zoekResultaat.ToString();
+            return View(zoekResultaat);
+        }
+        [NonAction]
+        public List<Woord> zoekWoorden(String search, string startDate, string endDate)
+        {
+            string[] splitSearch = search.Split(' ');
+            List<Woord> woorden = new List<Woord>();
+            foreach (string wrd in splitSearch)
+            {
+                if (wrd.ToLower().Equals("de") || wrd.ToLower().Equals("van") || wrd.ToLower().Equals("een") || wrd.ToLower().Equals("het"))
+                {
+                    continue;
+                }
+                else
+                {
+
+                    woorden.AddRange(berichtMng.GetWoorden().Where(w => w.Tekst.ToLower().Contains(wrd.ToLower()) && w.Berichten.FirstOrDefault(b => b.Datum > DateTime.Parse(startDate) && b.Datum < DateTime.Parse(endDate)) != null)
+                         .ToList());
+
+
+
+                }
+            }
+
+
+
+            return woorden;
+        }
+        [NonAction]
+        public List<Hashtag> zoekHashtags(String search, String startDate, String endDate)
+        {
+            string[] splitSearch = search.Split(' ');
+            List<Hashtag> hashtags = new List<Hashtag>();
+            foreach (string wrd in splitSearch)
+            {
+                if (wrd.ToLower().Equals("de") || wrd.ToLower().Equals("van") || wrd.ToLower().Equals("een") || wrd.ToLower().Equals("het"))
+                {
+                    continue;
+                }
+                else
+                {
+                    hashtags.AddRange(berichtMng.GetHashtags().Where(h => h.Tekst.ToLower().Contains(wrd.ToLower()) && h.Berichten.FirstOrDefault(b => b.Datum > DateTime.Parse(startDate) && b.Datum < DateTime.Parse(endDate)) != null)
+                          .ToList());
+                }
+            }
+
+            return hashtags;
+        }
+
+        [NonAction]
+        public List<Mention> zoekMentions(String search, String startDate, String endDate)
+        {
+            string[] splitSearch = search.Split(' ');
+            List<Mention> mentions = new List<Mention>();
+            foreach (string wrd in splitSearch)
+            {
+                if (wrd.ToLower().Equals("de") || wrd.ToLower().Equals("van") || wrd.ToLower().Equals("een") || wrd.ToLower().Equals("het"))
+                {
+                    continue;
+                }
+                else
+                {
+
+                    mentions.AddRange(berichtMng.GetMentions().Where(m => m.Tekst.ToLower().Contains(wrd.ToLower()) && m.Berichten.FirstOrDefault(b => b.Datum > DateTime.Parse(startDate) && b.Datum < DateTime.Parse(endDate)) != null)
+                          .ToList());
+
+
+                }
+            }
+            return mentions;
+        }
+        [NonAction]
+        public List<Url> zoekUrls(String search, String startDate, String endDate)
+        {
+            string[] splitSearch = search.Split(' ');
+            List<Url> urls = new List<Url>();
+            foreach (string wrd in splitSearch)
+            {
+                if (wrd.ToLower().Equals("de") || wrd.ToLower().Equals("van") || wrd.ToLower().Equals("een") || wrd.ToLower().Equals("het"))
+                {
+                    continue;
+                }
+                else
+                {
+
+                    urls.AddRange(berichtMng.GetUrls().Where(u => u.Tekst.ToLower().Contains(wrd.ToLower()) && u.Berichten.FirstOrDefault(b => b.Datum > DateTime.Parse(startDate) && b.Datum < DateTime.Parse(endDate)) != null)
+                          .ToList());
+                }
+            }
+            return urls;
+        }
+        [NonAction]
+        public List<Grafiek> zoekGrafieken(String search, String startDate, String endDate)
+        {
+            string[] splitSearch = search.Split(' ');
+            List<Grafiek> grafieken = new List<Grafiek>();
+            foreach (string wrd in splitSearch)
+            {
+                if (wrd.ToLower().Equals("de") || wrd.ToLower().Equals("van") || wrd.ToLower().Equals("een") || wrd.ToLower().Equals("het"))
+                {
+                    continue;
+                }
+                else
+                {
+                    grafieken.AddRange(grafiekMng.GetGrafieken().Where(g => g.GebruikerId.Equals(int.Parse(this.User.Identity.GetUserId())) && g.Titel.ToLower().Contains(wrd.ToLower()) && g.Personen.Where(p => p.Berichten.Where(b => b.Datum > DateTime.Parse(startDate) && b.Datum < DateTime.Parse(endDate)) != null) != null)
+                          .ToList());
+                    List<Persoon> personen = zoekPersonen(search, startDate, endDate);
+                    foreach (Persoon persoon in personen)
+                    {
+                        List<Grafiek> grafiekTussen = grafiekMng.GetGrafieken().Where(g => g.GebruikerId.Equals(int.Parse(this.User.Identity.GetUserId())) && g.Personen.Contains(persoon) && g.Personen.Where(p => p.Berichten.Where(b => b.Datum > DateTime.Parse(startDate) && b.Datum < DateTime.Parse(endDate)) != null) != null)
+                          .ToList();
+                        foreach (Grafiek gr in grafiekTussen)
+                        {
+                            if (grafieken.Contains(gr))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                grafieken.Add(gr);
+                            }
+                        }
+
+
+
+                    }
+                }
+            }
+            return grafieken;
+        }
+        [NonAction]
+        public List<Persoon> zoekPersonen(String search, String startDate, String endDate)
+        {
+            List<Persoon> personen = berichtMng.GetPersonen().Where(p => p.Naam.ToLower().Contains(search.ToLower()) && p.Berichten.FirstOrDefault(b => b.Datum > DateTime.Parse(startDate) && b.Datum < DateTime.Parse(endDate)) != null)
+                          .ToList();
+
+            return personen;
+        }
+
+
     }
-
-    public virtual ActionResult Search(string search)
-    {
-      if (search == null)
-      {
-        return PartialView("_Resultaat");
-      }
-
-      List<Persoon> personen = berichtMng.GetPersonen().Where(p => p.Naam.ToLower().Contains(search.ToLower()))
-          .ToList();
-
-
-      string[] splitSearch = search.Split(' ');
-      List<Woord> woorden = new List<Woord>();
-      List<Hashtag> hashtags = new List<Hashtag>();
-      List<Mention> mentions = new List<Mention>();
-      List<Url> urls = new List<Url>();
-      // List<Thema> themas = new List<Thema>();
-
-      List<Bericht> berichten = new List<Bericht>();
-      Bericht zoekresultaat = new Bericht();
-      foreach (string wrd in splitSearch)
-      {
-        woorden.AddRange(berichtMng.GetWoorden().Where(w => w.Tekst.ToLower().Contains(wrd.ToLower()))
-            .ToList());
-        hashtags.AddRange(berichtMng.GetHashtags().Where(h => h.Tekst.ToLower().Contains(wrd.ToLower()))
-            .ToList());
-        mentions.AddRange(berichtMng.GetMentions().Where(m => m.Tekst.ToLower().Contains(wrd.ToLower()))
-            .ToList());
-        urls.AddRange(berichtMng.GetUrls().Where(u => u.Tekst.ToLower().Contains(wrd.ToLower())).ToList());
-      }
-
-      //personen.Sort();
-      zoekresultaat.Woorden = woorden;
-      zoekresultaat.Hashtags = hashtags;
-      zoekresultaat.Mentions = mentions;
-      zoekresultaat.Personen = personen;
-      zoekresultaat.Urls = urls;
-      //Sortering testSortering = new Sortering();
-
-
-
-      woorden.ToString();
-      return View(zoekresultaat);
-      //return PartialView("_Resultaat",zoekresultaat);
-    }
-  }
 }
