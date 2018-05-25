9a6108125c41377cef311cb6bae4e066b4639e8e
unknown <brentvermolen@hotmail.be>
2018-05-25 22:15:21 +0200

    androidcontroller

--- MVCIntegratie/Controllers/Api/AndroidController.cs
+++ MVCIntegratie/Controllers/Api/AndroidController.cs
@@ -35,15 +35,15 @@ namespace MVCIntegratie.Controllers.Api
       [Route("~/api/Android/Grafieken/{platform}/{id}")]
       public IHttpActionResult GetGrafieken(string platform, string id)
       {
-         Deelplatform p = PlatformMng.GetDeelplatform(platform);
+         Deelplatform p = PlatformMng.GetDeelplatform(1);
 
          if (int.Parse(id) == 0)
          {
-            return Ok(GrafiekenMng.GetGrafieken().Where(g => g.Deelplatform.ID == p.ID && g.isDefault == true));
+            return Ok(GrafiekenMng.GetGrafieken(false).Where(g => g.Deelplatform.ID == p.ID && g.isDefault == true));
          }
          else
          {
-            return Ok(GrafiekenMng.GetGrafieken().Where(g => g.Deelplatform.ID == p.ID && g.GebruikerId == int.Parse(id)));
+            return Ok(GrafiekenMng.GetGrafieken(false).Where(g => g.Deelplatform.ID == p.ID && g.GebruikerId == int.Parse(id)));
          }
       }
 
@@ -54,12 +54,29 @@ namespace MVCIntegratie.Controllers.Api
          
          if (UserMng.CheckPassword(user, wachtwoord))
          {
-            return Ok(user);
+            return Ok(GebruikerMng.GetGebruiker(user.Id));
          }
          else
          {
             return NotFound();
          }
+      }
+
+      [Route("~/api/Android/EditGebruiker")]
+      public IHttpActionResult PostEditGebruiker([FromBody] Gebruiker gebruiker)
+      {
+         gebruiker.ToString();
+
+         Gebruiker gebruikerDb = GebruikerMng.GetGebruiker(gebruiker.ID);
+
+         gebruikerDb.UserName = gebruiker.UserName;
+         gebruikerDb.Email = gebruiker.Email;
+         gebruikerDb.Geboortedatum = gebruiker.Geboortedatum;
+         gebruikerDb.Postcode = gebruiker.Postcode;
+
+         GebruikerMng.ChangeGebruiker(gebruikerDb);
+
+         return Ok(true);
       }
    }
 }
